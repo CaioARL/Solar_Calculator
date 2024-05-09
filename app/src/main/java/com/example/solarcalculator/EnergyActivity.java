8d698f58848b4af8a42f6b1b8150b0c09d5bd722
Caio <caioarrudar.l2003@gmail.com>
2024-05-09 19:01:16 -0300

    Criando spinner para periodos de tempo

--- app/src/main/java/com/example/solarcalculator/EnergyActivity.java
+++ app/src/main/java/com/example/solarcalculator/EnergyActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -24,6 +26,8 @@ public class EnergyActivity extends AppCompatActivity {
 
     Button btnBack;
 
+    Spinner periodoMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,13 +42,17 @@ public class EnergyActivity extends AppCompatActivity {
         preferences = getSharedPreferences("saved_info", Context.MODE_PRIVATE);
 
         preco = findViewById(R.id.editPreco);
-        preco.setHint("R$" +  String.valueOf(preferences.getFloat("preco", 0.50F)));
+        preco.setHint("R$" + preferences.getFloat("preco", 0.50F));
         btnSave = findViewById(R.id.btnSaveEnergy);
         btnBack = findViewById(R.id.btnBack);
+        periodoMenu = findViewById(R.id.spinnerPeriodos);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.periodos, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        periodoMenu.setAdapter(adapter);
 
         btnSave.setOnClickListener(v -> {
             setPreco(convertPreco(preco));
-            preco.setHint("R$" +  String.valueOf(preferences.getFloat("preco", 0.50F)));
+            preco.setHint("R$" + preferences.getFloat("preco", 0.50F));
             Toast.makeText(this, "Preço Saved", Toast.LENGTH_SHORT).show();
         });
 
